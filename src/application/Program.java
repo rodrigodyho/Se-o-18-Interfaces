ca365e97d409368546a94c10d0c0497bb1b471a7
Rodrigo Lima <rodrigodyho@gmail.com>
2020-04-14 10:29:03 -0300

    Solution with abstract class only

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,46 +1,20 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
-
-import model.entities.Contract;
-import model.entities.Installment;
-import model.services.ContractService;
-import model.services.PayPalService;
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.entities.Shape;
+import model.enums.Color;
 
 public class Program {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Shape s1 = new Circle(Color.BLACK, 2.0);
+        Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 
-        System.out.println("Enter contract data");
-        System.out.print("Number: ");
-        int number = sc.nextInt();
-        System.out.print("Date (dd/MM/yyyy): ");
-        Date date = sdf.parse(sc.next());
-        System.out.print("Contract value: ");
-        double totalValue = sc.nextDouble();
-
-        Contract contract = new Contract(number, date, totalValue);
-
-        System.out.print("Enter number of installments: ");
-        int n = sc.nextInt();
-
-        ContractService contractService = new ContractService(new PayPalService());
-
-        contractService.processContract(contract, n);
-
-        System.out.println("Installments:");
-        for (Installment x : contract.getInstallments()) {
-            System.out.println(x);
-        }
-
-        sc.close();
+        System.out.println("Circle color: " + s1.getColor());
+        System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+        System.out.println("Rectangle color: " + s2.getColor());
+        System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
     }
 }
