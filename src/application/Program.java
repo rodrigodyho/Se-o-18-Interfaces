790613a553096e6c5c994d1d39a319faa35e7913
Rodrigo Lima <rodrigodyho@gmail.com>
2020-04-14 10:54:31 -0300

    Herança Multpla - WithOut Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,18 @@
 package application;
 
-import model.entities.AbstractShape;
-import model.entities.Circle;
-import model.entities.Rectangle;
-import model.enums.Color;
+import devices.Printer;
+import devices.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        AbstractShape s1 = new Circle(Color.BLACK, 2.0);
-        AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+        Printer p = new Printer("1080");
+        p.processDoc("My Letter");
+        p.print("My Letter");
 
-        System.out.println("Circle color: " + s1.getColor());
-        System.out.println("Circle area: " + String.format("%.3f", s1.area()));
-        System.out.println("Rectangle color: " + s2.getColor());
-        System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+        Scanner s = new Scanner("2003");
+        s.processDoc("My Email");
+        System.out.println("Scan result: " + s.scan());
     }
 }
